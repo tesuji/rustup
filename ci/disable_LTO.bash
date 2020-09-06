60bee2dee81bc4d0ef764c126e2fbadba8aea003
Lzu Tao <taolzu@gmail.com>
2020-09-06 20:26:34 +0700

    1 script

---
+++ ci/disable_LTO.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# Disable LTO=fat for non-"stable" builds
+# Why?
+# On my local machine:
+#   - a LTO build (roughly) takes 3m42s.
+#   - a non-LTO build takes 1m10s.
+# FIXME: Use stabilized cargo named-profiles on futures
+# https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#custom-named-profiles
+if [[ $(git rev-parse --abbrev-ref HEAD) != stable || $GITHUB_REF != refs/heads/stable ]]; then
+  sed -i 's@^lto@#lto@; s@^codegen@#codegen@' Cargo.toml
+  echo '[-] codegen options after changed'
+  sed -e '1,/\[profile/d' Cargo.toml
+fi
+
+# Force use of lld to improve linking time
+cat >> Cargo.toml << EOF
+[target.${TARGET}]
+linker = "lld-link"
+EOF
--- ci/run.bash
+++ ci/run.bash
@@ -13,25 +13,6 @@ case "$(uname -s)" in
   * ) FEATURES=('--features' 'vendored-openssl') ;;
 esac
 
-# Disable LTO=fat for non-"stable" builds
-# Why?
-# On my local machine:
-#   - a LTO build (roughly) takes 3m42s.
-#   - a non-LTO build takes 1m10s.
-# FIXME: Use stabilized cargo named-profiles on futures
-# https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#custom-named-profiles
-if [[ $(git rev-parse --abbrev-ref HEAD) != stable || $GITHUB_REF != refs/heads/stable ]]; then
-  sed -i 's@^lto@#lto@; s@^codegen@#codegen@;' Cargo.toml
-  echo '[-] codegen after changed'
-  sed -e '1,/\[profile/d' Cargo.toml
-fi
-
-# Force use of lld to improve linking time
-cat >> Cargo.toml << EOF
-[target.${TARGET}]
-linker = "lld-link"
-EOF
-
 # rustc only supports armv7: https://forge.rust-lang.org/release/platform-support.html
 if [ "$TARGET" = arm-linux-androideabi ]; then
   export CFLAGS='-march=armv7'

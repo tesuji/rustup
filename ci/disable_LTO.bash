c04b3ab30365481e8e17c407abd6f7009ea194c6
Lzu Tao <taolzu@gmail.com>
2020-09-06 20:58:46 +0700

    oh mac sed

--- ci/disable_LTO.bash
+++ ci/disable_LTO.bash
@@ -9,8 +9,8 @@ set -ex
 #   - a non-LTO build takes 1m10s.
 # FIXME: Use stabilized cargo named-profiles on futures
 # https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#custom-named-profiles
-if [[ $(git rev-parse --abbrev-ref HEAD) != stable || $GITHUB_REF != refs/heads/stable ]]; then
-  sed -i ".ori" 's@^lto@#lto@; s@^codegen@#codegen@; ' Cargo.toml
+if [[ $GITHUB_REF != refs/heads/stable || $(git rev-parse --abbrev-ref HEAD) != stable ]]; then
+  sed --in-place=".ori" -e 's@^lto@#lto@; s@^codegen@#codegen@; ' Cargo.toml
   echo '[-] codegen options after changed'
   sed -e '1,/\[profile/d' -- Cargo.toml
 fi
